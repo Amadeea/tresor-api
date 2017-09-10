5182ffcd8c3d8fe999583bdaa4c4e84e56e9e604
Amadeea <amadeakristina@gmail.com>
2017-09-10 19:01:11 +0700

    in progress get transaction list

--- db.js
+++ db.js
@@ -12,6 +12,28 @@ class DbWrapper{
         connection.query("INSERT INTO `users`(`email`, `password`) VALUES (\""+username+"\",\""+password+"\")");
         connection.end();
     }
+
+    login(username, password){
+        connection.connect();
+
+        connection.end();
+    }
+
+    getTransaction(userId){
+        connection.connect();
+        var result = connection.query("SELECT * FROM `transactions` WHERE `user_id`="+userId);
+        console.log(result);
+        connection.end();
+        return result;
+    }
+
+    addTransaction(transactionId){
+
+    }
+
+    updateTransaction(transactionId){
+
+    }
 }
 
 module.exports = DbWrapper;
--- index.js
+++ index.js
@@ -8,10 +8,21 @@ const dbWrapper   = new DbWrapper();
 
 app.use(bodyParser.urlencoded());
 
-app.post('/register', function(req,res){
+app.post('/register', function(req, res){
     var password = crypto.createHash('md5').update(req.body.password).digest("hex");
     dbWrapper.register(req.body.username, password);
     res.send("Data successfully registered");
+});
+
+app.post('/login', function(req, res){
+
+});
+
+app.get('/transaction', function(req, res){
+    var result = dbWrapper.getTransaction(req.param('user_id'));
+    res.send("Selesai");
+    // res.send(result);
+    // res.send(req.param('user_id'));
 });
 
 app.listen(3000, function(){
