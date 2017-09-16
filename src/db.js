c83a93fe576327b6f1428999b016dbce8fba4355
sebastianuskh <sebastianuskh@outlook.co.id>
2017-09-16 16:54:46 +0700

    uncomment db.js in root

--- src/db.js
+++ src/db.js
@@ -1,38 +1,38 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'tresor'
-// });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'tresor'
+});
 
-// class DbWrapper{
-//     register(username, password){
-//         connection.connect();
-//         connection.query("INSERT INTO `users`(`email`, `password`) VALUES (\""+username+"\",\""+password+"\")");
-//         connection.end();
-//     }
+class DbWrapper{
+    register(username, password){
+        connection.connect();
+        connection.query("INSERT INTO `users`(`email`, `password`) VALUES (\""+username+"\",\""+password+"\")");
+        connection.end();
+    }
 
-//     login(username, password){
-//         connection.connect();
+    login(username, password){
+        connection.connect();
 
-//         connection.end();
-//     }
+        connection.end();
+    }
 
-//     getTransaction(userId){
-//         connection.connect();
-//         var result = connection.query("SELECT * FROM `transactions` WHERE `user_id`="+userId);
-//         console.log(result);
-//         connection.end();
-//         return result;
-//     }
+    getTransaction(userId){
+        connection.connect();
+        var result = connection.query("SELECT * FROM `transactions` WHERE `user_id`="+userId);
+        console.log(result);
+        connection.end();
+        return result;
+    }
 
-//     addTransaction(transactionId){
+    addTransaction(transactionId){
 
-//     }
+    }
 
-//     updateTransaction(transactionId){
+    updateTransaction(transactionId){
 
-//     }
-// }
+    }
+}
 
-// module.exports = DbWrapper;
+module.exports = DbWrapper;
